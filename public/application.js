21ccf52ee04ea6953b7a28583ed8cb689212ec5a
Monica Olinescu <monica.olinescu@gmail.com>
2016-11-30 11:48:19 -0800

    Passing messages around

--- public/application.js
+++ public/application.js
@@ -1,5 +1,8 @@
 // Code will run on the client.
 
+// As the user for their name so we can send it to the server
+var username = prompt("Please enter your name")
+
 
 // Establish a websocket connection to the server.
 var socket = new WebSocket("ws://localhost:4000");
@@ -7,15 +10,33 @@ var socket = new WebSocket("ws://localhost:4000");
 // Listen to when the connection is open.
 socket.onopen = (e) => {
   console.log("Connected to server...")
+
+  // You can start sending messages.
+  // Tell the server this user's username
+  var message = { type: 'set_name', username: username}
+  // convert the message to a JSON string and send it over
+  socket.send(JSON.stringify(message))
 }
 
 // Listen in to messages from the server and console.log them
 
 socket.onmessage = (event) => {
   console.log('event data', event.data)
+  var message = JSON.parse(event.data)
+  if(message.type === 'picked_tile') {
+    $("#" + message.tileId).css({"background-color": message.color});
+  }
 }
 
 socket.onclose = (event) => {
   console.log('Connection closed.')
 }
 
+$(function() { // wait for DOM to load
+  $("td").on("click", function(e) {
+    var tileId = $(this).attr("id")
+    var message = { type: 'picked_tile', tileId: tileId }
+    socket.send(JSON.stringify(message))
+  })
+
+})
--- server.js
+++ server.js
@@ -13,15 +13,46 @@ const wss = new SocketServer({ server });
 wss.on('connection', (socket) => {
   // every client has their own socket
   console.log("Client connected... ")
+  socket.color = getRandomColor();
 
   // wait 3 seconds and say hello
 
   setTimeout(() => {
-    socket.send("hello")
+    socket.send(JSON.stringify({ type: "hello"}))
   }, 2000)
+
+  // listen in for messages
+  socket.on('message', (message) => {
+    message = JSON.parse(message);
+    console.log('Got message', message)
+
+    if(message.type === 'set_name') {
+      // Keep track of the user who owns the socket
+      socket.currentUser = message.username;
+    } else if(message.type === 'picked_tile') {
+      // let all clients know that a tile was picked.
+      // broadcast.
+      wss.clients.forEach((clientSocket) => {
+        var upgradedMessage = {
+          type: 'picked_tile',
+          tileId: message.tileId,
+          username: socket.username,
+          color: socket.color
+        }
+        clientSocket.send(JSON.stringify(upgradedMessage))
+      })
+
+    }
+  })
 
 
 })
+
+
+function getRandomColor() {
+  var colors = ['red', 'tomato', 'green', 'chartreuse', 'blue', 'black']
+  return colors[Math.floor(Math.random()*colors.length)]
+}
 
 
 
